f75280e76491e890c99f9f1d0319d1b358a697fc
Jeromy Cannon <jeromy@swirldslabs.com>
2025-04-30 16:44:02 -0700

    save
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- src/version_two_config.py
+++ src/version_two_config.py
@@ -2,13 +2,82 @@ import argparse
 
 
 class VersionTwoConfig:
-    parser = argparse.ArgumentParser(
-        prog='VersionTwo',
-        description="Render an HTML page from a collection of GitHub Issues "
-                    "and Pull Requests",
-    )
+    def __init__(self):
+        parser = argparse.ArgumentParser(
+            prog='VersionTwo',
+            description="Render an HTML page from a collection of GitHub Issues"
+                        "and Pull Requests",
+        )
 
-    parser.add_argument()
+        parser.add_argument(
+            "--include-organization",
+            dest="include_org",
+            action="append_const",
+            help="Include all issues and PRs from the specified organization",)
+
+        parser.add_argument(
+            "--include-repository",
+            dest="include_repository",
+            action="append_const",
+            help="Include all issues and PRs from the specified repository",)
+
+        parser.add_argument(
+            "--include-organization-repository",
+            dest="include_organization_repository",
+            action="append_const",
+            help="Include all issues and PRs from the specified "
+                 "organization/repository",)
+
+        parser.add_argument(
+            "--include-user",
+            dest="include_user",
+            action="append_const",
+            help="Include all issues and PRs for the provided user",)
+
+        parser.add_argument(
+            "--include-label",
+            dest="include_label",
+            action="append_const",
+            help="Include all issues and PRs with the specified label",)
+
+        parser.add_argument(
+            "--exclude-organization",
+            dest="include_org",
+            action="append_const",
+            help="Exclude all issues and PRs from the specified organization",)
+
+        parser.add_argument(
+            "--exclude-repository",
+            dest="include_repository",
+            action="append_const",
+            help="Exclude all issues and PRs from the specified repository",)
+
+        parser.add_argument(
+            "--exclude-organization-repository",
+            dest="include_organization_repository",
+            action="append_const",
+            help="Exclude all issues and PRs from the specified "
+                 "organization/repository",)
+
+        parser.add_argument(
+            "--exclude-user",
+            dest="include_user",
+            action="append_const",
+            help="Exclude all issues and PRs for the provided user",)
+
+        parser.add_argument(
+            "--exclude-label",
+            dest="include_label",
+            action="append_const",
+            help="Exclude all issues and PRs with the specified label",)
+
+        parser.add_argument(
+            "--publish-board",
+            dest="publish_board",
+            action="append_const",
+            help="The organization/board to publish (add) the collection of "
+                 "GitHub Issues and Pull Requests",)
+
 
 
 
