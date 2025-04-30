319d9e138d11f8784c431d2a8c0e45e09905635f
Jeromy Cannon <jeromy@swirldslabs.com>
2025-04-30 16:12:37 -0700

    save
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

---
+++ src/version_two_config.py
@@ -0,0 +1,14 @@
+import argparse
+
+
+class VersionTwoConfig:
+    parser = argparse.ArgumentParser(
+        prog='VersionTwo',
+        description="Render an HTML page from a collection of GitHub Issues "
+                    "and Pull Requests",
+    )
+
+    parser.add_argument()
+
+
+
