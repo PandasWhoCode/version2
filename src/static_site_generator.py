8d9c0aeb05b99fdf20c40d7f3908550d4b889e5c
dpswirld <diogo.pereira@swirldslabs.com>
2025-05-01 09:43:29 -0700

    feat: Added github JSON loader and Fixed JS Search (#45)
    Signed-off-by: Diogo Pereira <diogo.pereira@swirldslabs.com>
    Signed-off-by: dpswirld <diogo.pereira@swirldslabs.com>
    Signed-off-by: Roger Barker <roger.barker@swirldslabs.com>
    Co-authored-by: Roger Barker <roger.barker@swirldslabs.com>
    Co-authored-by: Andrew Brandt <andrew.brandt@hashgraph.com>

--- src/static_site_generator.py
+++ src/static_site_generator.py
@@ -1,28 +1,34 @@
 from jinja2 import Environment, FileSystemLoader
+import logging 
 
-# Example data
-GENERATOR_DATA = {
-    'title': 'Version Two',
-    'github_user': 'octocat',
-    'team': 'Platform Engineering',
-    'tasks': []
-}
 
-def generate_site(data=None, output_file='./_site/index.html'):
-    env = Environment(loader=FileSystemLoader('templates'))
-    template = env.get_template('kaban_board.html')
+class StaticSiteGenerator():
 
-    GENERATOR_DATA["tasks"] = data if data is not None else []
+    def __init__(self):
+        self.GENERATOR_DATA = {
+            'title': 'Version Two',
+            'github_user': 'octocat',
+            'team': 'Platform Engineering',
+            'tasks': []
+        }
 
-    # Extract unique statuses from the tasks list
-    unique_statuses = sorted({task["status"] for task in GENERATOR_DATA["tasks"]})
-    
-    # Add statuses to the data dictionary
-    data_with_statuses = {**GENERATOR_DATA, "statuses": unique_statuses}
-    
-    # Render template with updated data
-    output = template.render(data_with_statuses)
-    
-    with open(output_file, 'w') as f:
-        f.write(output)
+    def generate_site(self, data=None, output_file='./_site/index.html'):
+        logging.info("Generating Static Site")
+        env = Environment(loader=FileSystemLoader('templates'))
+        template = env.get_template('kaban_board.html')
 
+        self.GENERATOR_DATA["tasks"] = data if data is not None else []
+
+        # Extract unique statuses from the tasks list
+        unique_statuses = sorted({task["status"] for task in self.GENERATOR_DATA["tasks"]})
+        
+        # Add statuses to the data dictionary
+        data_with_statuses = {**self.GENERATOR_DATA, "statuses": unique_statuses}
+        
+        # Render template with updated data
+        output = template.render(data_with_statuses)
+        
+        with open(output_file, 'w') as f:
+            f.write(output)
+
+        logging.info("Static Site Generated @ ./_site/index.html")
